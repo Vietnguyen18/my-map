8c0450c9ac68e13af40550ba8d60edd2a1233f5a
Vietnguyen18 <viet82073@gmail.com>
2024-06-17 17:08:47 +0700

    update #3

--- src/component/MapWithOverlayAndSlider.js
+++ src/component/MapWithOverlayAndSlider.js
@@ -1,65 +1,63 @@
 import React, { useState } from 'react';
-import { GoogleMap, LoadScript, Marker, GroundOverlay } from '@react-google-maps/api';
-import anh from '../image/anh_mau.jpg'
+import { MapContainer, TileLayer, Marker, ImageOverlay, useMap } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import anh from '../image/anh_mau.jpg';
+import '../component/map.css';
 
 const containerStyle = {
-  position: 'relative',
   width: '100%',
-  height: '800px'
+  height: '90vh',
 };
 
-const center = {
-  lat: 21.145,
-  lng: 105.849
-};
+const dongAnhBounds = [
+  [21.224, 105.7215],
+  [21.0540, 105.952] 
+];
 
-const imageURL = anh;
+const UpdateMapView = ({ center }) => {
+  const map = useMap();
+  map.setView(center, 13);
+  return null;
+};
 
 const MapWithOverlayAndSlider = () => {
   const [opacity, setOpacity] = useState(0);
+  const imageURL = anh;
+  
 
   const handleOpacityChange = (event) => {
     setOpacity(parseFloat(event.target.value));
   };
 
   return (
-    <div>
-      <LoadScript googleMapsApiKey="AIzaSyBa7UlmsSGVz7NA2HkBdfevTBiwIPP2mdY">
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={10}
-        >
-          {window.google && window.google.maps ? (
-            <>
-              <Marker
-                position={center}
-              />
-              <GroundOverlay 
-                url={imageURL}
-                bounds={{
-                  north: center.lat + 0.01,
-                  south: center.lat - 0.01,
-                  east: center.lng + 0.01,
-                  west: center.lng - 0.01
-                }}
-                opacity={opacity}
-              />
-            </>
-          ) : null}
-        </GoogleMap>
-        <div style={{ position: 'absolute', top: '130px', left: '83px', transform: 'translateX(-60%)', zIndex: 1, backgroundColor: 'white', padding: '1px', borderRadius: '5px' }}>
-          <input
-            type="range"
-            id="opacitySlider"
-            min="0"
-            max="1"
-            step="0.1"
-            value={opacity}
-            onChange={handleOpacityChange}
+    <div style={{ position: 'relative' }}>
+      <MapContainer style={containerStyle} center={[21.175, 105.827]} zoom={13}>
+        <UpdateMapView center={[21.175, 105.827]} />
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[21.175, 105.827]} />
+        {imageURL && (
+          <ImageOverlay
+            url={imageURL}
+            bounds={dongAnhBounds}
+            opacity={opacity}
           />
-        </div>
-      </LoadScript>
+        )}
+      </MapContainer>
+      <div style={{ position: 'absolute', top: '10px', left: '50%', transform: 'translateX(-50%)', zIndex: 1000, backgroundColor: 'white', padding: '10px', borderRadius: '5px' }}>
+        <label htmlFor="opacitySlider">Điều chỉnh độ trong suốt của ảnh:</label>
+        <input
+          type="range"
+          id="opacitySlider"
+          min="0"
+          max="1"
+          step="0.1"
+          value={opacity}
+          onChange={handleOpacityChange}
+        />
+      </div>
     </div>
   );
 };
