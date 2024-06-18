8cf7be900f24b2bf5224378401f96065d74c3436
Vietnguyen18 <viet82073@gmail.com>
2024-06-18 12:08:24 +0700

    #update 4

--- src/component/MapWithOverlayAndSlider.js
+++ src/component/MapWithOverlayAndSlider.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { MapContainer, TileLayer, Marker, ImageOverlay, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import anh from '../image/anh_mau.jpg';
+import { pdfjs } from 'react-pdf';
 import '../component/map.css';
 
 const containerStyle = {
@@ -10,8 +10,8 @@ const containerStyle = {
 };
 
 const dongAnhBounds = [
-  [21.228, 105.695],
-  [21.0260, 105.966] 
+  [21.229, 105.6652],
+  [21.0290, 105.980] 
 ];
 
 const UpdateMapView = ({ center }) => {
@@ -22,11 +22,46 @@ const UpdateMapView = ({ center }) => {
 
 const MapWithOverlayAndSlider = () => {
   const [opacity, setOpacity] = useState(0);
-  const imageURL = anh;
-  
+  const [imageURL, setImageURL] = useState(null);
 
   const handleOpacityChange = (event) => {
     setOpacity(parseFloat(event.target.value));
+  };
+
+  const handleFileChange = async (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      try {
+        const pdfFile = URL.createObjectURL(file);
+
+        // Using pdfjs library to render PDF pages as images
+        pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+        const loadingTask = pdfjs.getDocument(pdfFile);
+        const pdf = await loadingTask.promise;
+
+        // Render only the first page of the PDF as an image
+        const page = await pdf.getPage(1);
+        const scale = 1.5;
+        const viewport = page.getViewport({ scale });
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        const renderContext = {
+          canvasContext: context,
+          viewport: viewport
+        };
+
+        await page.render(renderContext).promise;
+        const imageData = canvas.toDataURL('image/png');
+
+        setImageURL(imageData);
+      } catch (error) {
+        console.error('Error rendering PDF:', error);
+      }
+    }
   };
 
   return (
@@ -57,6 +92,9 @@ const MapWithOverlayAndSlider = () => {
           value={opacity}
           onChange={handleOpacityChange}
         />
+        <label>{(opacity * 100).toFixed(0)} %</label>
+
+        <input type="file" accept="application/pdf" onChange={handleFileChange} style={{ marginLeft: '10px' }} />
       </div>
     </div>
   );
